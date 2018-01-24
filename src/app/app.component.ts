5e2ef083b84927d88b98c00c8f9ecdfc75d691ef
WingedDante <logan.trecartin@gmail.com>
2018-01-24 13:21:47 -0500

    update with nav and poke details, and styling edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,23 +23,69 @@ export class AppComponent {
   search_progress = 0;
   show_progress = false;
   types: string[];
-
+  poke: Pokemon;
+  error: string;
   constructor(private pService: PokemonService){
     this.pokemon_names =  pService.get_all_pokemon();
   }
 
   change_poke (name:string){
+    this.error = null;
+    // console.log(this.pokemon_names.indexOf(name.toLowerCase()));
+    if (this.pokemon_names.indexOf(name.substring(0,1).toUpperCase() + name.substring(1,name.length).toLowerCase()) <0){
+      this.poke = null;
+      this.error = 'Pokemon not found';
+      
+    }
     //console.log(this.pService.get_all_pokemon());
     this.show_progress = true;
     this.search_progress = 50;
-    this.pokemon_name = name;
+    //this.pokemon_name = name;
     this.pService.get_pokemon(name).subscribe(data =>{
+      this.poke = new Pokemon();
+      this.poke.stats={
+        total: 0,
+        hp: 0,
+        attack: 0,
+        defense: 0,
+        spAttack: 0,
+        spDefense: 0,
+        speed: 0
+      };
+      this.poke.name = name;
+      this.poke.types = [];
       this.search_progress= 100;
       this.pokemon_name = name;
       this.pokemon_url = data['sprites']['front_default'];
-      this.types = [];
+      for(var stat = 0; stat < data['stats'].length; stat++){
+        this.poke.stats.total += data['stats'][stat].base_stat;
+        
+        switch (data['stats'][stat].stat.name) {
+          case 'speed':
+            this.poke.stats.speed = data['stats'][stat].base_stat
+            break;
+          case 'special-defense':
+          this.poke.stats.spDefense = data['stats'][stat].base_stat
+            break;
+          case 'special-attack':
+          this.poke.stats.spAttack = data['stats'][stat].base_stat
+            break;
+          case 'attack':
+          this.poke.stats.attack = data['stats'][stat].base_stat
+            break;
+          case 'defense':
+          this.poke.stats.defense = data['stats'][stat].base_stat
+            break;
+          case 'hp':
+          this.poke.stats.hp = data['stats'][stat].base_stat
+            break;
+          default:
+            break;
+        }
+      };
+
       data['types'].forEach(element => {
-        this.types.push(element['type']['name']);
+        this.poke.types.push(element['type']['name']);
       });
             // console.log(p);
             // return p;
@@ -63,6 +109,49 @@ export class AppComponent {
     // this.show_progress = false;
   // });
   }
+
+  getStyle = (item) => {
+    switch (item) {
+      case 'psychic':
+        return '#F85888';
+      case 'grass':
+        return '#78C850';
+      case 'normal':
+        return '#A8A878';
+      case 'fighting':
+        return '#C03028';
+      case 'flying':
+        return '#A890F0';
+      case 'poison':
+        return '#A040A0';
+      case 'ground':
+        return '#E0C068';
+      case 'rock':
+        return '#B8A038';
+      case 'bug':
+        return '#A8B820';
+      case 'ghost':
+        return '#705898';
+      case 'steel':
+        return '#B8B8D0';
+      case 'fire':
+        return '#F08030';
+      case 'water':
+        return '#6890F0';
+      case 'electric':
+        return '#F8D030';
+      case 'ice':
+        return '#98D8D8';
+      case 'dragon':
+        return '#7038F8';
+      case 'dark':
+        return '#705848';
+      case 'fairy':
+        return '#EE99AC';
+      default:
+        return '#cccccc';
+    }
+  };
 
   search = (text$: Observable<string>) =>
     text$
--- src/models/pokemon.model.ts
+++ src/models/pokemon.model.ts
@@ -2,4 +2,14 @@ export class Pokemon {
     name: string;
     types: string[];
     image_url: string;
+    stats: {
+        total: number,
+        hp: number,
+        attack: number,
+        defense: number,
+        spAttack: number,
+        spDefense: number, 
+        speed: number
+    }
+    
 }
